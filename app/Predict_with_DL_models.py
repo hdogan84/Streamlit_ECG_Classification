1c91b4a73a3161a2a7afe0d1d4d47b50c4f4a085
Simon Dommer <simon.dommer@googlemail.com>
2024-04-04 21:25:45 +0200

    Changed the entire structure of DL Page 2: It is now done with a Predictions class instead of functions. Continuing with the all_results dictionary to make beautiful plots and comparisons...

--- app/Predict_with_DL_models.py
+++ app/Predict_with_DL_models.py
@@ -1,4 +1,3 @@
-# Core Pkg
 import streamlit as st
 from functions import load_datasets_in_workingspace, predict_with_DL
 from sklearn.metrics import classification_report, confusion_matrix
@@ -6,146 +5,134 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
 
 
-def Page_DL_Stage_2(data_path = "../data/heartbeat"):
-    
-    st.title("Predicting with Deep learning (DL) models")
+class DLModelPredictor:
+    def __init__(self):
+        #self.all_results = {}
+        self.data_path = "../data/heartbeat"
+        #initializing all_results from the session state if given, else this will be an empty dictionnary
+        if 'all_results' not in st.session_state:
+            st.session_state['all_results'] = {}
+        self.all_results = st.session_state['all_results']
 
-    #Here some dynamic overviews are needed! with checkboxes to hide them.
-    # --> Overview on model structures: Call build function and then model.summary?
-    # --> Overview on experiment design: Make a table in functions.py or complete function just for the table and show it? Or use picture at first from the report!
+    def generate_results(self, dataset_name, dataset_to_use, selected_models, selected_experiments,
+                        comparison_method, selected_sampling, num_classes):
+        results = {}
 
-    #choose the dataset --> Multiselection, so comparison of different datasets is also possible
-    dataset_names = st.multiselect("Select Datasets (more than one option possible)", ["MITBIH", "PTBDB"])
+        for model_name in selected_models:
+            for experiment in selected_experiments:
+                model_path = f"../assets/DL_Models/{model_name}/experiment_{experiment}_{dataset_name}_{selected_sampling}.weights.h5"
 
-    #choose the sampling method (Only original and B_SMOTE should be available)
-    selected_sampling = st.selectbox("Select the sampling method on which the models were trained", ["A_Original", "B_SMOTE"])
-    #choose the models --> Multiselection
-    model_options = ["Simple_ANN", "Simple_CNN", "Advanced_CNN"]
-    selected_models = st.multiselect("Select the DL models (more than one option possible)", options=model_options)
+                if comparison_method == "Single Row (Random)":
+                    row_index = np.random.randint(len(dataset_to_use))
+                    single_row = pd.DataFrame(dataset_to_use.iloc[row_index].values.reshape(1, -1))
+                    y_true = dataset_to_use.iloc[row_index, 187]
+                    prediction, report = predict_with_DL(test=single_row, model=model_name, model_path=model_path,
+                                                         show_conf_matr=False, num_classes=num_classes)
+                    results[f"{model_name}_Exp{experiment}"] = report
+                    st.dataframe(report)
 
-    #choose the experiment that was used to train the models, also multiselection to make it fun.
-    #this needs an (dynamic!) overview figure of the experiments, so that the user has knowledge what this means
-    experiment_options = ["1", "2", "3", "4"] #does this make sense with strings or are integers preferable?
-    selected_experiments = st.multiselect("Select the experiments the models were trained on (more than one option possible)", options=experiment_options)
+                elif comparison_method == "Complete Dataset":
+                    prediction, report = predict_with_DL(test=dataset_to_use, model=model_name, model_path=model_path,
+                                                         show_conf_matr=False, num_classes=num_classes)
+                    y_true = dataset_to_use[187].values
+                    results[f"{model_name}_Exp{experiment}"] = report
+                    st.dataframe(report)
 
-    #choose the comparison method (only one selection possible)
-    comparison_method = st.radio("Select the comparison method", ["Single Row (Random)", "Complete Dataset"])
+        if comparison_method == "Complete Dataset":
+            for result_key, result in results.items():
+                cm = confusion_matrix(y_true, prediction)
+                fig, ax = plt.subplots()
+                sns.heatmap(cm, annot=True, fmt="d", ax=ax, cmap="Blues")
+                ax.set_title(f"Confusion Matrix for {result_key} and Dataset {dataset_name}_{selected_sampling}")
+                ax.set_xlabel('Predicted Labels')
+                ax.set_ylabel('True Labels')
+                st.pyplot(fig)
 
-    all_results = {} #empty dictionary, that is used in either of the two branches below
-    if len(dataset_names) > 1:
-        st.warning("Comparing between datasets is selected.")
-        st.info("Please note that direct comparison metrics may not be meaningful across different datasets.")
-        for dataset_name in dataset_names:
-            st.subheader(f"Results for {dataset_name} Dataset")
-            mitbih_test, mitbih_train, ptbdb_abnormal, ptbdb_normal = load_datasets_in_workingspace()
-            if dataset_name == "MITBIH":
-                dataset_to_use = mitbih_test
-                num_classes = 5
+        return results
+
+    def display_classification_report(self):
+        #for result_key, result in self.all_results.items(): #this can also not work, because self.all_results is lost anytime something is updated.
+        for result_key, result in st.session_state['all_results'].items():
+            st.subheader(f"{result_key}")
+            st.write("Classification Report:")
+            if isinstance(result, dict):
+                for class_name, metrics in result.items():
+                    if class_name in ['accuracy', 'macro avg', 'weighted avg']:
+                        continue
+                    st.write(f"Class: {class_name}")
+                    df = pd.DataFrame(metrics, index=[0])
+                    st.dataframe(df)
+
+    def run(self):
+        st.title("Predicting with Deep learning (DL) models")
+
+        dataset_names = st.multiselect("Select Datasets (more than one option possible)", ["MITBIH", "PTBDB"])
+        selected_sampling = st.selectbox("Select the sampling method on which the models were trained",
+                                         ["A_Original", "B_SMOTE"])
+        model_options = ["Simple_ANN", "Simple_CNN", "Advanced_CNN"]
+        selected_models = st.multiselect("Select the DL models (more than one option possible)", options=model_options)
+        experiment_options = ["1", "2", "3", "4"]
+        selected_experiments = st.multiselect("Select the experiments the models were trained on (more than one option possible)",
+                                              options=experiment_options)
+        comparison_method = st.radio("Select the comparison method", ["Single Row (Random)", "Complete Dataset"])
+        
+        if st.button("Generate Results"):
+            if len(dataset_names) > 1:
+                st.warning("Comparing between datasets is selected.")
+                st.info("Please note that direct comparison metrics may not be meaningful across different datasets.")
+                for dataset_name in dataset_names:
+                    st.subheader(f"Results for {dataset_name} Dataset")
+                    mitbih_test, mitbih_train, ptbdb_abnormal, ptbdb_normal = load_datasets_in_workingspace()
+                    if dataset_name == "MITBIH":
+                        dataset_to_use = mitbih_test
+                        num_classes = 5
+                    else:
+                        dataset_to_use = pd.concat([ptbdb_abnormal, ptbdb_normal], ignore_index=True).sample(frac=1,
+                                                                                                                 random_state=42)
+                        num_classes = 2
+                    results = self.generate_results(dataset_name, dataset_to_use, selected_models, selected_experiments,
+                                                    comparison_method, selected_sampling, num_classes)
+                    self.all_results[dataset_name] = results
+                    #now we overwrite the current session state with the calculated all_results, this is necessary in streamlit, because it overwrites all variables each time a button is pressed...
+                    st.session_state['all_results'] = self.all_results
+                    st.write("Here comes the debugging inside of if st.button(generate_results) for more than one dataset....")
+                    st.write(self.all_results)
             else:
-                #here the concatenting and reshuffling is done
-                dataset_to_use = pd.concat([ptbdb_abnormal, ptbdb_normal], ignore_index=True).sample(frac=1, random_state=42)
-                num_classes = 2
-            results = generate_results(dataset_name, dataset_to_use, selected_models, selected_experiments, comparison_method, selected_sampling, num_classes)
-            # Store results in a dictionary with dataset_name as key
-            all_results[dataset_name] = results
+                for dataset_name in dataset_names:
+                    st.subheader(f":green[Results for {dataset_name} Dataset]")
+                    mitbih_test, mitbih_train, ptbdb_abnormal, ptbdb_normal = load_datasets_in_workingspace()
+                    if dataset_name == "MITBIH":
+                        dataset_to_use = mitbih_test
+                        num_classes = 5
+                    else:
+                        dataset_to_use = pd.concat([ptbdb_abnormal, ptbdb_normal], ignore_index=True).sample(frac=1,
+                                                                                                                 random_state=42)
+                        num_classes = 2
 
-    else:
-        for dataset_name in dataset_names:
-            st.subheader(f":green[Results for {dataset_name} Dataset]")
-            mitbih_test, mitbih_train, ptbdb_abnormal, ptbdb_normal = load_datasets_in_workingspace()
-            if dataset_name == "MITBIH":
-                dataset_to_use = mitbih_test
-                num_classes = 5 #we can simply choose the num_classes here isntead of overcomplicating things?
-            else:
-                #here the concatenting and reshuffling is done
-                dataset_to_use = pd.concat([ptbdb_abnormal, ptbdb_normal], ignore_index=True).sample(frac=1, random_state=42)
-                num_classes = 2
-            
-            results = generate_results(dataset_name, dataset_to_use, selected_models, selected_experiments, comparison_method, selected_sampling, num_classes)
-            # Store results in a dictionary with dataset_name as key
-            all_results[dataset_name] = results            
-            """y_true = dataset_to_use[187].values
-            
-            if comparison_method == "Single Row (Random)":
-                row_index = np.random.randint(len(dataset_to_use))
-                single_row = pd.DataFrame(dataset_to_use.iloc[row_index].values.reshape(1, -1)) #has to be dataframe for predict_with_dl function, even if its only single row.
-                true_label = y_true[row_index]
-                
-                for model_name in selected_models:
-                    for experiment in selected_experiments:
-                        #this is the model path that should be reused in that way!
-                        model_path = f"../assets/DL_Models/{model_name}/experiment_{experiment}_{dataset_name}_{selected_sampling}.weights.h5"
-                        prediction, report = predict_with_DL(test=single_row, model=model_name, model_path=model_path, show_conf_matr=False, num_classes=num_classes) #conf matrix makes no sense for one prediction
-                        st.write(f"Model: {model_name}, Experiment: {experiment}, True Label: {true_label}, Predicted Label: {prediction}")
-                        st.write("Classification report")
-                        st.dataframe(report)
+                    results = self.generate_results(dataset_name, dataset_to_use, selected_models, selected_experiments,
+                                                    comparison_method, selected_sampling, num_classes)
+                    self.all_results[dataset_name] = results
+                    #now we overwrite the current session state with the calculated all_results, this is necessary in streamlit, because it overwrites all variables each time a button is pressed...
+                    st.session_state['all_results'] = self.all_results
+                    st.write("Here comes the debugging inside of if st.button(generate_results) for one dataset....")
+                    #st.write(self.all_results) #this is not working, because each time a button is pressed, self.all_results is lost in streamlit...
+                    st.write(st.session_state['all_results']) #this should work the same way as @st.cache_data (so not entirely like a global variable!)
+             
 
-            elif comparison_method == "Complete Dataset":
-                results = {}
-                for model_name in selected_models:
-                    for experiment in selected_experiments:
-                        #this is the model path that should be reused in that way!
-                        model_path = f"../assets/DL_Models/{model_name}/experiment_{experiment}_{dataset_name}_{selected_sampling}.weights.h5"
-                        prediction, report = predict_with_DL(test=dataset_to_use, model=model_name, model_path=model_path, show_conf_matr=False, num_classes=num_classes) #conf matrix is done below 
-                        st.write(f"Model: {model_name}, Experiment: {experiment}") #, True Label: {true_label}, Predicted Label: {prediction}
-                        st.write("Classification report")
-                        st.dataframe(report)
-                        results[f"{model_name}_Exp{experiment}"] = classification_report(y_true, prediction, output_dict=True)
+        if st.button("show all_results (debugging)"):
+            st.write("Here comes the debugging outside of if st.button(generate_results)....")
+            st.write(self.all_results)
 
-                for result_key, result in results.items():
-                    #st.subheader(f"{result_key}")
-                    #st.json(result)
+        display_options = st.multiselect("Select Display Option(s)",
+                                         ["Classification Report", "Predicted Label", "True Label", "Confusion Matrix"])
 
-                    # Confusion matrix, not done with dedicated function, but beautiful?
-                    cm = confusion_matrix(y_true, prediction)
-                    fig, ax = plt.subplots()
-                    sns.heatmap(cm, annot=True, fmt="d", ax=ax, cmap="Blues")
-                    ax.set_title(f"Confusion Matrix for {result_key} and Dataset {dataset_name}_{selected_sampling}")
-                    ax.set_xlabel('Predicted Labels')
-                    ax.set_ylabel('True Labels')
-                    st.pyplot(fig)"""
-    
-#If this works, the function should be outsourced in functions.py
-#but first: Make it work entirely and then rename it, so it doesnt get lost in functions.py
-@st.cache_data
-def generate_results(dataset_name, dataset_to_use, selected_models, selected_experiments, comparison_method, selected_sampling, num_classes):
-    results = {}
-    
-    for model_name in selected_models:
-        for experiment in selected_experiments:
-            model_path = f"../assets/DL_Models/{model_name}/experiment_{experiment}_{dataset_name}_{selected_sampling}.weights.h5"
+        if display_options == "Classification Report":
+            self.display_classification_report()
 
-            if comparison_method == "Single Row (Random)":
-                row_index = np.random.randint(len(dataset_to_use))
-                single_row = pd.DataFrame(dataset_to_use.iloc[row_index].values.reshape(1, -1)) #has to be dataframe for predict_with_dl function, even if its only single row.
-                y_true = dataset_to_use.iloc[row_index, 187]
-                prediction, report = predict_with_DL(test=single_row, model=model_name, model_path=model_path, show_conf_matr=False, num_classes=num_classes)
-                st.write(f"Model: {model_name}, Experiment: {experiment}, True Label: {y_true}, Predicted Label: {prediction}")
-                results[f"{model_name}_Exp{experiment}"] = report
-                st.write(f"Classification report for {model_name} and Experiment No. {experiment}")
-                st.dataframe(report)
 
-            elif comparison_method == "Complete Dataset":
-                prediction, report = predict_with_DL(test=dataset_to_use, model=model_name, model_path=model_path, show_conf_matr=False, num_classes=num_classes)
-                y_true = dataset_to_use[187].values
-                results[f"{model_name}_Exp{experiment}"] = report
-                st.write(f"Classification report for {model_name} and Experiment No. {experiment}")
-                st.dataframe(report)
-
-    #the plotting of the confusion matrizes is only done for complete dataset, otherwise it makes no sense. But even here, it doesnt provide too much benefit.
-    if comparison_method == "Complete Dataset":
-         for result_key, result in results.items():
-                    #st.subheader(f"{result_key}")
-                    #st.json(result)
-
-                    # Confusion matrix, not done with dedicated function, but beautiful?
-                    cm = confusion_matrix(y_true, prediction)
-                    fig, ax = plt.subplots()
-                    sns.heatmap(cm, annot=True, fmt="d", ax=ax, cmap="Blues")
-                    ax.set_title(f"Confusion Matrix for {result_key} and Dataset {dataset_name}_{selected_sampling}")
-                    ax.set_xlabel('Predicted Labels')
-                    ax.set_ylabel('True Labels')
-                    st.pyplot(fig)
-    return results
+def Page_DL_Stage_2():
+    predictor = DLModelPredictor()
+    predictor.run()
