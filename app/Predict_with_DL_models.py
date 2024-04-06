db977b150a9d9d5ec875132373f013e5b3972ff6
Simon Dommer <simon.dommer@googlemail.com>
2024-04-06 13:08:37 +0200

    all_results dictionary is now created successfully. display_classification_report is working, but not too beatiful and might need rework.

--- app/Predict_with_DL_models.py
+++ app/Predict_with_DL_models.py
@@ -12,7 +12,7 @@ def run():
     st.title("Predicting with Deep learning (DL) models")
 
     dataset_names = st.multiselect("Select Datasets (more than one option possible)", ["MITBIH", "PTBDB"])
-    selected_sampling = st.selectbox("Select the sampling method on which the models were trained",
+    selected_sampling = st.multiselect("Select the sampling method for the trainingset on which the models were trained",
                                      ["A_Original", "B_SMOTE"])
     model_options = ["Simple_ANN", "Simple_CNN", "Advanced_CNN"]
     selected_models = st.multiselect("Select the DL models (more than one option possible)", options=model_options)
@@ -38,32 +38,21 @@ def run():
                                                                           selected_experiments, comparison_method, selected_sampling, num_classes)
             all_results[dataset_name] = results
             st.session_state['all_results'] = all_results #otherwise everything will be lost...
-            #st.write("Here comes the debugging for the generated results....")
-            #st.write(all_results)
+           
         #debugging how the results dictionary looks like...
-        st.write("How does the all_results dictionary look like?")
-        st.write(all_results)
-        #These functions work, but they only use the last generated variable, so its not useful for multiselection of datasets. Therefore they should be executed, when
-        #button "show results" is pushed.
-        """if "Classification Report" in display_options:
-            st.write("Displaying Classification Report...")
-            display_classification_report(results)
-
-        if "Confusion Matrix" in display_options:
-            st.write("Displaying Confusion Matrix with single row results")
-            display_confusion_matrix(single_row_results)"""
-
+        #st.write("How does the all_results dictionary look like?")
+        #st.write(all_results)
+        st.info("The results have been generated, continuing with showing the results is possible.")
     if st.button("Show Results"):
-        st.write("Displaying Results...")
+        st.header("Results")
         all_results = st.session_state["all_results"] #since all_results is not put trough all buttons, it is newly created trough the persistend session_state variable...
-        #st.write(all_results) #only needed for debugging
-        
+               
         if "Classification Report" in display_options:
-            st.write("Displaying Classification Report...")
+            st.subheader("Classification Report")
             display_classification_report(all_results)
 
         if "Confusion Matrix" in display_options:
-            st.write("Displaying Confusion Matrix with single row results")
+            st.subheader("Confusion Matrices")
             display_confusion_matrix(all_results)
 
 #@st.cache_data #this function must not be cached, otherwise the randomness of the single row is lost?
@@ -76,30 +65,30 @@ def generate_results(dataset_name, dataset_to_use, selected_models, selected_exp
 
     for model_name in selected_models:
         results[model_name] = {} #initializing empty dictionary for each model_name (this is the same strategy as with all_results dictionary!)
-        for experiment in selected_experiments:
-            model_path = f"../assets/DL_Models/{model_name}/experiment_{experiment}_{dataset_name}_{selected_sampling}.weights.h5"
+        for sampling_method in selected_sampling:
+            results[model_name][sampling_method] = {}
+            for experiment in selected_experiments:
+                model_path = f"../assets/DL_Models/{model_name}/experiment_{experiment}_{dataset_name}_{sampling_method}.weights.h5"
+                if comparison_method == "Single Row (Random)":
+                    
+                    y_true = dataset_to_use.iloc[row_index, 187]
+                    prediction, report = predict_with_DL(test=single_row, model=model_name, model_path=model_path,
+                                                        show_conf_matr=False, num_classes=num_classes)
+                    results[model_name][sampling_method]["Experiment " + experiment] = {
+                        "y_true": y_true,
+                        "prediction": int(prediction[0]),
+                        "report": report
+                    }
 
-            if comparison_method == "Single Row (Random)":
-                
-                y_true = dataset_to_use.iloc[row_index, 187]
-                prediction, report = predict_with_DL(test=single_row, model=model_name, model_path=model_path,
-                                                     show_conf_matr=False, num_classes=num_classes)
-                #st.dataframe(report) #for debugging: Is a report available?
-                results[model_name]["Experiment " + experiment] = {
-                    "y_true": y_true,
-                    "prediction": int(prediction[0]),
-                    "report": report
-                }
-
-            elif comparison_method == "Complete Dataset":
-                prediction, report = predict_with_DL(test=dataset_to_use, model=model_name, model_path=model_path,
-                                                     show_conf_matr=False, num_classes=num_classes)
-                y_true = dataset_to_use[187].values
-                results[model_name]["Experiment " + experiment] = {
-                    "y_true": y_true,
-                    "prediction": prediction, #this could make trouble, but lets see
-                    "report": report
-                }
+                elif comparison_method == "Complete Dataset":
+                    prediction, report = predict_with_DL(test=dataset_to_use, model=model_name, model_path=model_path,
+                                                        show_conf_matr=False, num_classes=num_classes)
+                    y_true = dataset_to_use[187].values
+                    results[model_name][sampling_method]["Experiment " + experiment] = {
+                        "y_true": y_true,
+                        "prediction": prediction, #this could make trouble, but lets see
+                        "report": report
+                    }
     
     return results
 
@@ -110,12 +99,14 @@ def display_classification_report(results):
         st.subheader(f"Dataset {dataset_name}")
         for model_name, model_result in dataset_results.items():
             st.subheader(f"Model {model_name}")
-            for experiment, experiment_result in model_result.items():
-                st.subheader(f"{experiment}") #this is already correctly named due to new dictionary structure.
-                st.write("Classification Report:")
-                if isinstance(experiment_result, dict) and "report" in experiment_result:
-                    report = experiment_result["report"]
-                    st.dataframe(report)
+            for sampling_method, sampling_method_result in model_result.items():
+                st.subheader(f"Sampling Method training Set: {sampling_method}")
+                for experiment, experiment_result in sampling_method_result.items():
+                    st.subheader(f"{experiment}") #this is already correctly named due to new dictionary structure.
+                    st.write("Classification Report:")
+                    if isinstance(experiment_result, dict) and "report" in experiment_result:
+                        report = experiment_result["report"]
+                        st.dataframe(report)
 @st.cache_data
 def display_confusion_matrix(results):
     for dataset_name, single_row_class_report_results in results.items():
