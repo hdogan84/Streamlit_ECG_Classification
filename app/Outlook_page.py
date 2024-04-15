4f9bfd69fe66893582ea1c848d392c347aca3087
Simon Dommer <simon.dommer@googlemail.com>
2024-04-15 12:57:39 +0200

    Editet the outlook page.

--- app/Outlook_page.py
+++ app/Outlook_page.py
@@ -4,16 +4,15 @@ from functions import displayPDF # our own function to display pdf files
 
 def Outlook():
     st.header("Conclusions")
-    st.write("- ML and DL Models both performed very well in comparison with recent literature.")
-    st.write("- ML and DL Models both performed better on the PTBDB Dataset. This could be due to the smaller amount of classes to be distinquished.")
-    st.write("- :red[PLEASE FILL THIS WITH CONCLUSIONS FROM THE REPORT]")
-
-
+    st.write("- ML and DL models, particularly ANN and CNN, demonstrated strong performance, outperforming recent benchmarks in the literature.")
+    st.write("- The best performing model, CNN, achieved an accuracy of 98.57% on the MITBIH dataset and 99.59% on the PTBDB dataset.")
+    st.write("- The reliability and legal acceptance of using ML for critical medical applications remain in question. Establishing performance thresholds could be crucial for broader acceptance and to mitigate risks.")
+    
     st.header("Outlook")
-    st.write("The best DL Model in experiment 4 configuration can be deployed for the following application areas.")
-    st.write("- Assistance in classifying heartbeat patterns")
-    st.write("- long term observation of patients or during night shifts")
-    st.write("- Training of prospective medical staff via a website / app")
-    st.write("- :red[PLEASE FILL THIS WITH OUTLOOKS FROM THE REPORT OR YOUR OWN MIND AFTER PLAYING AROUND WITH THE DEMO]")
+    st.write("The best DL model from experiment 4 shows potential for deployment in various medical applications:")
+    st.write("- Assistance in classifying heartbeat patterns, useful for long-term patient observation or during night shifts.")
+    st.write("- Training of prospective medical staff through an interactive website or app.")
+    st.write("- To further enhance model performance and applicability, we recommend scaling the model using diverse datasets, adding more convolutional layers, and experimenting with novel data augmentation techniques.")
+    st.write("- Special attention should be given to improving precision for detecting abnormal heartbeat patterns, particularly by increasing sample sizes for underrepresented classes.")
     
     
